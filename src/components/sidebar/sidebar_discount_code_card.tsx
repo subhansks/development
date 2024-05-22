306fc5c0b9cbcaa159aae5e22b616a5fd354a1cd
AbSubhan <absubhansks@gmail.com>
2024-05-22 19:09:54 +0500

    little changes

--- src/components/sidebar/sidebar_discount_code_card.tsx
+++ src/components/sidebar/sidebar_discount_code_card.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import Discount_code_card_component from "./discount_code_card_component";
 
 const DiscountCodeCard = () => {
   return (
@@ -9,35 +10,11 @@ const DiscountCodeCard = () => {
         Utvalda rabattkoder & deals
       </h1>
       <div className=" shadow-lg shadow-gray-200  bg-dealguru-white w-full rounded-lg  mt-3">
-        <div className="p-3 flex gap-3 justify-start items-start">
-          {/* Card Image */}
-          <Link href={"#"}>
-            <Image
-              src="/deal1.jpg"
-              width={65}
-              height={65}
-              alt=""
-              className=" min-w-[65px]  min-h-[65px] rounded-xl "
-            />
-          </Link>
-          <div className=" flex flex-col gap-2  justify-start  ">
-            {/* title here */}
-            <p className=" text-base text-wrap leading-4 text-dealguru-black">
-              XXXLutz: Sign up for the newsletter and get a SEK 50 coupon!
-            </p>
-
-            {/* Card category */}
-            <div className="flex  gap-3  items-center">
-              <Link
-                className="w-[180px] bg-dealguru-blue text-white text-sm font-bold text-center p-1 rounded-md relative discount-code-button"
-                href=""
-              >
-                Visa rabattkod
-              </Link>
-            </div>
-          </div>
-        </div>
-        <hr className="mx-3 last_child_hr border-dealguru-grey" />
+        <Discount_code_card_component />
+        <Discount_code_card_component />
+        <Discount_code_card_component />
+        <Discount_code_card_component />
+        <Discount_code_card_component />
       </div>
     </div>
   );
--- src/components/sidebar/sidebar_fav_dealcard.tsx
+++ src/components/sidebar/sidebar_fav_dealcard.tsx
@@ -1,6 +1,5 @@
-import Image from "next/image";
-import Link from "next/link";
 import React from "react";
+import Fav_dealcard_component from "./fav_dealcard_component";
 
 const FavDealCard = () => {
   return (
@@ -9,45 +8,11 @@ const FavDealCard = () => {
         Utvalda favoriter
       </h1>
       <div className=" shadow-lg shadow-gray-200  bg-dealguru-white w-full rounded-lg  mt-3">
-        <div className="p-3 flex gap-3 justify-start items-center">
-          {/* Card Image */}
-          <Link href={"#"}>
-            <Image
-              src="/deal1.jpg"
-              width={65}
-              height={65}
-              alt=""
-              className=" min-w-[65px]  min-h-[65px] rounded-xl "
-            />
-          </Link>
-          <div className=" flex flex-col gap-2  justify-start  overflow-hidden ">
-            {/* title here */}
-            <p className=" text-base  truncate">
-              Rituals Skincare Quiz - get free sample more sample
-            </p>
-
-            {/* Card category */}
-            <div className="flex  gap-3  items-center">
-              <Link className="" href={"#"}>
-                <p className="text-sm">Rituals</p>
-              </Link>
-              <div className="border-l h-5 border-gray-300" />
-              <div className=" ">
-                <div className="flex gap-1 ">
-                  <Image
-                    src="/like.svg"
-                    width={14}
-                    height={14}
-                    alt="like"
-                    className=""
-                  />
-                  <span className="font-bold text-gray-400 text-sm">234</span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <hr className="mx-3 last_child_hr border-dealguru-grey" />
+        <Fav_dealcard_component />
+        <Fav_dealcard_component />
+        <Fav_dealcard_component />
+        <Fav_dealcard_component />
+        <Fav_dealcard_component />
       </div>
     </div>
   );
