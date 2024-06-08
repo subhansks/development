029cc09ae28455bc147f6ffb351b950892c3b53b
AbSubhan <absubhansks@gmail.com>
2024-06-08 19:43:20 +0500

    custom slider same like hukd done

--- src/app/testing/page.tsx
+++ src/app/testing/page.tsx
@@ -1,19 +1,5 @@
-import { Button } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogContent,
- 
-  DialogTrigger,
-} from "@/components/ui/dialog";
-
+import ScrollSlider from "@/components/homepage/custom_slider";
 
 export default function testing() {
-  return (
-    <Dialog>
-      <DialogTrigger className="mt-60" asChild>
-        <Button variant="outline">Edit Profile</Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]  ">Teting</DialogContent>
-    </Dialog>
-  );
+  return <ScrollSlider />;
 }
