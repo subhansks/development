436e634e00b40e918d755172c70ec78b691a2f99
durraniarts <abdurrehmandurrani1215@gmail.com>
2024-06-25 20:57:08 +0500

    discussion , add comment, discussion details, extended and to top button added

--- src/app/(Discussion_pages)/components/layout/Discussion_Comment_Section.tsx
+++ src/app/(Discussion_pages)/components/layout/Discussion_Comment_Section.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import Discussion_Comment_Section_Structure from '../modules/Discussion_Comment_Section_Structure';
+import React from "react";
+import Discussion_Comment_Section_Structure from "../modules/Discussion_Comment_Section_Structure";
 
 const Discussion_Comment_Section = ({ details }: any | undefined) => {
-    return (
-        <div className='md_lg:col-span-5 xl:col-span-6  border-gray-300 rounded-lg  '>
-            <Discussion_Comment_Section_Structure details={details} />
+  return (
+    <div className="md_lg:col-span-5 xl:col-span-7  border-gray-300 rounded-lg  ">
+      <Discussion_Comment_Section_Structure details={details} />
 
-            {/* <button className='border-dealguru-blue border rounded-lg py-2 px-4 mt-8 w-full bg-transparent font-light text-dealguru-blue'>Visa fler deals</button> */}
-
-        </div>
-    );
+      {/* <button className='border-dealguru-blue border rounded-lg py-2 px-4 mt-8 w-full bg-transparent font-light text-dealguru-blue'>Visa fler deals</button> */}
+    </div>
+  );
 };
 
 export default Discussion_Comment_Section;
