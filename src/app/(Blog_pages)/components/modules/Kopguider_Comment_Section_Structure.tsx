bca611d88ab2f2f7586b97fd90caafd4a1e45545
usmanwaheed <usmanwaheed.dev@gmail.com>
2024-06-27 13:42:57 +0500

    fixed pull request

--- src/app/(Blog_pages)/components/modules/Kopguider_Comment_Section_Structure.tsx
+++ src/app/(Blog_pages)/components/modules/Kopguider_Comment_Section_Structure.tsx
@@ -1,13 +1,13 @@
 import React, { Suspense } from "react";
-import Main_Discussion_card from "../common/Main_Discussion_card";
+import Main_Kopguider_card from "../common/Main_Kopguider_card";
 import Rich_Text_editor from "../common/Rich_Text_editor";
 import Loader from "../common/Loader";
 import Comments_Card from "../common/Comments_Card";
 
-const Discussion_Comment_Section_Structure = ({ details }: any | undefined) => {
+const Kopguider_Comment_Section_Structure = ({ details }: any | undefined) => {
   return (
     <section className="flex flex-col gap-6">
-      <Main_Discussion_card details={details} />
+      <Main_Kopguider_card details={details} />
 
       <Suspense fallback={<Loader />}>
         <Rich_Text_editor />
@@ -17,4 +17,4 @@ const Discussion_Comment_Section_Structure = ({ details }: any | undefined) => {
   );
 };
 
-export default Discussion_Comment_Section_Structure;
+export default Kopguider_Comment_Section_Structure;
