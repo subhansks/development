9a490a0206dcbeab89dc7b873cb0a16b588bc3d3
usmanwaheed <usmanwaheed.dev@gmail.com>
2024-07-09 14:19:56 +0500

    blog pages responsive fixes

--- src/app/(Blog_pages)/components/layout/KopguiderSection.tsx
+++ src/app/(Blog_pages)/components/layout/KopguiderSection.tsx
@@ -4,13 +4,13 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const KopguiderSection = ({ details }: any | undefined) => {
   return (
-    <section className='md:w-[85%] w-[100%] mx-auto'>
+    <section className='w-[100%] mx-auto'>
       <div className='flex items-center flex-col gap-4 w-full'>
         <div className='flex sm:items-center justify-between w-full sm:flex-row flex-col gap-4 items-start'>
           <Tabs defaultValue="newest" className="flex flex-col w-full items-center justify-center gap-2 py-2 rounded-md">
             <div className='flex w-full justify-between'>
               <h3 className='text-2xl font-semibold text-gray-800 mobile-hidden pt-5'>Köpguider</h3>
-              <TabsList className='bg-white mob-w-full'>
+              <TabsList className='bg-white mob-w-full dealguru-bottom-navigation-main'>
                 <TabsTrigger value="newest">Nya</TabsTrigger>
                 <TabsTrigger value="popular">Populära</TabsTrigger>
               </TabsList>
