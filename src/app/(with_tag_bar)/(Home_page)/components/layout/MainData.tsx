1dd5e7569f87a2ef3b3ff9ab7a89047669185c03
Abd Ur Rehman Durrani <100535468+durraniarts@users.noreply.github.com>
2024-07-23 13:09:28 +0500

    Update MainData.tsx

--- src/app/(with_tag_bar)/(Home_page)/components/layout/MainData.tsx
+++ src/app/(with_tag_bar)/(Home_page)/components/layout/MainData.tsx
@@ -1,15 +1,16 @@
 import HomePageContent from "@/components/homepage/homePage";
+import { DATA } from "@/constants/data";
 
 // import { cookies } from "next/headers";
 import React from "react";
 
 const MainData = async () => {
-  const response = await fetch("http://localhost:3000/api/home/", {
-    next: { tags: ["data"], revalidate: 20 },
-  });
-  const res = await response.json();
+  // const response = await fetch("http://localhost:3000/api/home/", {
+  //   next: { tags: ["data"], revalidate: 20 },
+  // });
+  // const res = await response.json();
 
-  const [topRecommendationData, leastRecommendedData] = res.message;
+  const [topRecommendationData, leastRecommendedData] = DATA[0];
   console.log(topRecommendationData);
   return (
     <>
